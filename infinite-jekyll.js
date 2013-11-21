6a6716c1decbe3f64fa5bd8b8ec90eb61fdf033f
Tobias Ahlin <hello@tobiasahlin.com>
2013-11-21 23:03:33 +0100

    .js

---
+++ infinite-jekyll.js
@@ -0,0 +1,78 @@
+$(function() {
+  
+	var postURLs,
+      isFetchingPosts = false,
+			shouldFetchPosts = true,
+			postsToLoad = $(".posts").children().length,
+			loadNewPostsThreshold = 3000;
+	
+  // Load the JSON file containing all URLs
+	$.getJSON('/all-posts.json', function(data) {
+		postURLs = data["posts"];
+	});
+	
+	// If there's no spinner, it's not a page where posts should be fetched
+	if ($(".infinite-spinner").length < 1)
+		shouldFetchPosts = false; 
+	
+	// Are we close to the end of the page? If we are, load more posts
+	$(window).scroll(function(e){
+		if (!shouldFetchPosts || isFetchingPosts) return;
+		
+		var windowHeight = $(window).height();
+		var windowScrollPosition = $(window).scrollTop();
+		var bottomScrollPosition = windowHeight + windowScrollPosition;
+		var documentHeight = $(document).height();
+		
+    // If we've scrolled past the loadNewPostsThreshold, fetch posts
+		if ((documentHeight - loadNewPostsThreshold) < bottomScrollPosition) {
+			fetchPosts();
+		}
+	});
+  
+  // Fetch a chunk of posts
+	function fetchPosts() {
+    // Exit if postURLs haven't been loaded
+    if (!postURLs) return;
+    
+		isFetchingPosts = true;
+		
+    // Load as many posts as there were present on the page when it loaded
+    // After successfully loading a post, load the next one
+		var loadedPosts = 0,
+				postCount = $(".posts").children().length,
+				callback = function() {
+					loadedPosts++;
+					var postIndex = postCount + loadedPosts;
+					
+					if (postIndex > postURLs.length-1) {
+						disableFetching();
+						return;
+					}
+					
+					if (loadedPosts < postsToLoad) {
+						fetchPostWithIndex(postIndex, callback);
+					} else {
+						isFetchingPosts = false;
+					}
+				};
+		
+		fetchPostWithIndex(postCount + loadedPosts, callback);
+	}
+	
+	function fetchPostWithIndex(index, callback) {
+		var postURL = postURLs[index];
+		
+		$.get(postURL, function(data) {
+			$(data).find(".post").appendTo(".blog-posts");
+			callback();
+		});
+	}
+	
+	function disableFetching() {
+		shouldFetchPosts = false;
+		isFetchingPosts = false;
+		$(".infinite-spinner").fadeOut();
+	}
+	
+});
